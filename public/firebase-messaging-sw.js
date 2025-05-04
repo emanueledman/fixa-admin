23db8649bfca582d1c216cbc62109055caddaa52
Emanuel Joao <emanuelhedima@gmail.com>
2025-05-04 13:53:49 +0100

    fdh

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVtY6ML3j-qrIsAprIJPB5xFFCbcf4UQw",
+  authDomain: "facilita-479b3.firebaseapp.com",
+  databaseURL: "https://facilita-479b3-default-rtdb.firebaseio.com",
+  projectId: "facilita-479b3",
+  storageBucket: "facilita-479b3.firebasestorage.app",
+  messagingSenderId: "385676676886",
+  appId: "1:385676676886:web:hpv5p5d4onvvosgglv5lf4337bv2bojc"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icons/Icon-192.png'
+  };
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
